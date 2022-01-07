f6c04950d55002f7031b168d6cfef9a916189868
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-01-07 11:03:05 +0100

    0.0.3

--- Sources/MockMealAdapters/MockGetMealsCommand.swift
+++ Sources/MockMealAdapters/MockGetMealsCommand.swift
@@ -9,6 +9,10 @@ import Foundation
 import MealPorts
 
 public class MockGetMealsCommand : GetMealsCommand {
+    
+    public init() {
+    }
+    
     public func execute(inDTO: MealQuery) async throws -> Array<MealCollection> {
         return [MockMealCollection(name: "Dishes", meals: dishes),
                 MockMealCollection(name: "Desserts", meals: desserts)]
